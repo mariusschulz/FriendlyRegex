52e4519822a8abcf22c1a61be368020f067f91d2
Marius Schulz <marius.schulz@me.com>
2014-06-19 17:57:37 +0200

    Supporting more anchors now

--- FriendlyRegex/Anchor.cs
+++ FriendlyRegex/Anchor.cs
@@ -3,10 +3,15 @@
     internal class Anchor : RegularExpression
     {
         public static readonly Anchor StartOfStringOrLine;
+        public static readonly Anchor StartOfString;
+
         public static readonly Anchor EndOfStringOrLine;
+        public static readonly Anchor EndOfString;
+        public static readonly Anchor VeryEndOfString;
 
         public static readonly Anchor WordBoundary;
         public static readonly Anchor NonWordBoundary;
+        public static readonly Anchor EndPositionOfPreviousMatch;
 
         private readonly string _symbol;
 
@@ -18,10 +23,16 @@
         static Anchor()
         {
             StartOfStringOrLine = new Anchor("^");
+            StartOfString = new Anchor(@"\A");
+
             EndOfStringOrLine = new Anchor("$");
+            EndOfString = new Anchor(@"\Z");
+            VeryEndOfString = new Anchor(@"\z");
 
             WordBoundary = new Anchor(@"\b");
             NonWordBoundary = new Anchor(@"\B");
+
+            EndPositionOfPreviousMatch = new Anchor(@"\G");
         }
 
         public override string GetStringRepresentation()
